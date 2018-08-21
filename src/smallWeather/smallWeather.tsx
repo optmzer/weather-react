9bf062c93f78f4b4ea582e11890c4f51aab8ff25
Alexander Frolov <big_air2409@mail.ru>
2018-08-21 23:18:21 +1200

    Added MatUI for a SmallWeather component. Works OK.

--- src/smallWeather/smallWeather.tsx
+++ src/smallWeather/smallWeather.tsx
@@ -1,3 +1,4 @@
+import { ButtonBase, Grid, Paper, Typography } from "@material-ui/core";
 import * as React from "react";
 import "./smallWeather.css";
 
@@ -15,19 +16,40 @@ export interface ISmallWeatherProps {
 
 // "HelloProps" describes the shape of props.
 // State is never set so we use the "{}" type.
-export default class SmallWeather extends React.Component<any, {}> {
-    // constructor(props: any){
-    //     super(props);
-    // }
+export default class SmallWeather extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
 
     public render() {
         return(
             <div className="small-weather">
-                <img className="small-weather-img" src={this.props.data.iconSrc}/>
-                <div className="small-weather-metric">
-                    <p className="small-weather-temp">{this.props.data.temp}</p>
-                    <p className="small-weather-date">{this.props.data.date}</p>
-                </div>
+                <Paper className="weather-tile">
+                    <Grid container={true} spacing={16}>
+                        <Grid item={true}>
+                            <ButtonBase className="image">
+                                <img className="weather-icon" alt="complex" src={this.props.data.iconSrc} />
+                            </ButtonBase>
+                        </Grid>
+                        <Grid item={true} xs={12} sm={true} container={true}>
+                            <Grid item={true} xs={true} container={true} direction="column" spacing={16}>
+                                <Grid item={true} xs={true}>
+                                <Typography gutterBottom={true} variant="subheading">
+                                    Standard license
+                                </Typography>
+                                <Typography gutterBottom={true}>Full resolution 1920x1080 • JPEG</Typography>
+                                <Typography color="textSecondary">ID: 1030114</Typography>
+                                </Grid>
+                                <Grid item={true}>
+                                <Typography style={{ cursor: "pointer" }}>Remove</Typography>
+                                </Grid>
+                            </Grid>
+                            <Grid item={true}>
+                                <Typography variant="subheading">$19.00</Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Paper>
             </div>
         ); // return()
     }// render()
